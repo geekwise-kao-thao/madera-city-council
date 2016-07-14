4a3e16d5b04d98596d530dfff0dea0ad5cbeb4f6
Kao Thao <kausthoj@gmail.com>
2016-07-14 23:38:37 +0000

    cleaning code, working on css documentation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
+/**
+ * Added small javascript code to easily assist CSS with media query.
+ * 
+ * When window screen size is changing, the container for title and name
+ * will readjust its height to the height of the image for a cleaner look.
+*/
+
 document.addEventListener('DOMContentLoaded',function(event){
     
     var mayor_first_last_container = document.getElementById('mayor_first_last_container');
     var the_mayor_picture = document.getElementById('the_mayor_picture');
     
     window.addEventListener('resize',function(event){
-        
-        console.log(document.body.offsetWidth);
        
         if(document.body.offsetWidth >= 513){
             mayor_first_last_container.style.height = the_mayor_picture.height + 'px';
